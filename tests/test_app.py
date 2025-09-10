5e8cea907e9f7d992d28b672f67506240902cfc9
Jason-blend <ikaminas@blendtechnologies.com>
2025-09-10 13:12:50 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,5 @@
-# test_app.py using unittest
-import unittest
-from app import app
-
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.client = app.test_client()
-
-    def test_homepage(self):
-        response = self.client.get("/notes")
-        self.assertEqual(response.status_code, 200)
-
-if __name__ == "__main__":
-    unittest.main()
+stage('Unit Tests') {
+    steps {
+        sh './venv/bin/python -m pytest tests'
+    }
+}
