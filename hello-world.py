696701242f15841d80675f6b5ab08027b0f38918
Jason-blend <ikaminas@blendtechnologies.com>
2025-09-10 11:38:25 +0300

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,6 @@
-print("Hello World your first pipeline is running!")
+from app import app
+
+def test_homepage():
+    client = app.test_client()
+    response = client.get("/notes")
+    assert response.status_code == 200
