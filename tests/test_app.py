378ba0340ea795e7e7cf21236e1a82c9faa522bb
Jason-blend <ikaminas@blendtechnologies.com>
2025-09-10 13:29:52 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
-stage('Unit Tests') {
-    steps {
-        sh './venv/bin/python -m pytest tests'
-    }
-}
+#from app import app
+
+#def test_homepage():
+#    client = app.test_client()
+#    response = client.get("/notes")
+#    assert response.status_code == 200
