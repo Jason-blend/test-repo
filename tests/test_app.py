02540ce54f27c029a031caf8500c997b4ea81f97
Jason-blend <ikaminas@blendtechnologies.com>
2025-09-10 13:07:25 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,14 @@
+# test_app.py using unittest
+import unittest
 from app import app
 
-def test_homepage():
-    client = app.test_client()
-    response = client.get("/notes")
-    assert response.status_code == 200
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_homepage(self):
+        response = self.client.get("/notes")
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
